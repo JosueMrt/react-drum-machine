f5e8972abb7814af9937b2d5df37f95968681ac4
axe i-grec <tag4rce7@gmail.com>
2020-04-03 22:27:27 +0200

    Updates

---
+++ precache-manifest.f4a29c12386fbaca20fee3905303ab5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48306d06b41b03b4ada3d57cb15cf0e8",
+    "url": "/react-drum-machine/index.html"
+  },
+  {
+    "revision": "3a73c04134b183ff01bf",
+    "url": "/react-drum-machine/static/css/main.387a3f59.chunk.css"
+  },
+  {
+    "revision": "2f33959741ad0580e366",
+    "url": "/react-drum-machine/static/js/2.222d7939.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-drum-machine/static/js/2.222d7939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a73c04134b183ff01bf",
+    "url": "/react-drum-machine/static/js/main.1b0a5390.chunk.js"
+  },
+  {
+    "revision": "ffb67937b124cd498eae",
+    "url": "/react-drum-machine/static/js/runtime-main.2d6c58ae.js"
+  }
+]);
